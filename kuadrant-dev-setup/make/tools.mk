ccdf02b590c02446c6d1d4ed41a5042123a73042
Eguzki Astiz Lezaun <eastizle@redhat.com>
2025-12-03 10:47:06 +0100

    Makefile refactor: make/tools.mk
    Signed-off-by: Eguzki Astiz Lezaun <eastizle@redhat.com>

---
+++ kuadrant-dev-setup/make/tools.mk
@@ -0,0 +1,57 @@
+##@ Tools
+
+HELM_VERSION ?= v3.15.0
+HELM = $(LOCALBIN)/helm-$(HELM_VERSION)
+
+# install helm
+$(LOCALBIN)/helm-$(HELM_VERSION): $(LOCALBIN)
+	@if [ -f "$@" ]; then \
+		echo "helm $(HELM_VERSION) already exists at $@"; \
+	else \
+		echo "downloading helm $(HELM_VERSION)..."; \
+		if [ "$$(uname -s)" = "Darwin" ]; then \
+			if [ "$$(uname -m)" = "arm64" ]; then \
+				curl -Lo $(LOCALBIN)/helm.tar.gz https://get.helm.sh/helm-$(HELM_VERSION)-darwin-arm64.tar.gz; \
+			else \
+				curl -Lo $(LOCALBIN)/helm.tar.gz https://get.helm.sh/helm-$(HELM_VERSION)-darwin-amd64.tar.gz; \
+			fi; \
+		else \
+			if [ "$$(uname -m)" = "aarch64" ]; then \
+				curl -Lo $(LOCALBIN)/helm.tar.gz https://get.helm.sh/helm-$(HELM_VERSION)-linux-arm64.tar.gz; \
+			else \
+				curl -Lo $(LOCALBIN)/helm.tar.gz https://get.helm.sh/helm-$(HELM_VERSION)-linux-amd64.tar.gz; \
+			fi; \
+		fi; \
+		tar -zxvf $(LOCALBIN)/helm.tar.gz -C $(LOCALBIN); \
+		if [ "$$(uname -s)" = "Darwin" ]; then \
+			mv $(LOCALBIN)/darwin-*/helm $@; \
+		else \
+			mv $(LOCALBIN)/linux-*/helm $@; \
+		fi; \
+		rm -rf $(LOCALBIN)/helm.tar.gz $(LOCALBIN)/darwin-* $(LOCALBIN)/linux-*; \
+		chmod +x $@; \
+	fi
+
+KIND_VERSION ?= v0.20.0
+KIND = $(LOCALBIN)/kind-$(KIND_VERSION)
+
+$(LOCALBIN)/kind-$(KIND_VERSION): $(LOCALBIN)
+	@if [ -f "$@" ]; then \
+		echo "kind $(KIND_VERSION) already exists at $@"; \
+	else \
+		echo "downloading kind $(KIND_VERSION)..."; \
+		if [ "$$(uname -s)" = "Darwin" ]; then \
+			if [ "$$(uname -m)" = "arm64" ]; then \
+				curl -Lo $@ https://kind.sigs.k8s.io/dl/$(KIND_VERSION)/kind-darwin-arm64; \
+			else \
+				curl -Lo $@ https://kind.sigs.k8s.io/dl/$(KIND_VERSION)/kind-darwin-amd64; \
+			fi; \
+		else \
+			if [ "$$(uname -m)" = "aarch64" ]; then \
+				curl -Lo $@ https://kind.sigs.k8s.io/dl/$(KIND_VERSION)/kind-linux-arm64; \
+			else \
+				curl -Lo $@ https://kind.sigs.k8s.io/dl/$(KIND_VERSION)/kind-linux-amd64; \
+			fi; \
+		fi; \
+		chmod +x $@; \
+	fi
