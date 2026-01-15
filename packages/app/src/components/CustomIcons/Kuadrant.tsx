b5720c1a389652702af0aac18aeb849dfc233a46
Jason Madigan <jason@jasonmadigan.com>
2026-01-15 11:16:07 +0000

    icon tweak
    Signed-off-by: Jason Madigan <jason@jasonmadigan.com>

--- packages/app/src/components/CustomIcons/Kuadrant.tsx
+++ packages/app/src/components/CustomIcons/Kuadrant.tsx
@@ -1,14 +1,17 @@
-import { createSvgIcon } from '@mui/material/utils';
+import SvgIcon, { SvgIconProps } from '@mui/material/SvgIcon';
 
-const KuadrantIcon = createSvgIcon(
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024">
+const KuadrantIcon = (props: SvgIconProps) => (
+  <SvgIcon
+    {...props}
+    viewBox="0 0 1024 1024"
+    sx={{ transform: 'scale(0.85)', marginLeft: '4px', ...props.sx }}
+  >
     <path
       d="M809,1C993.59,183.56 1065.7,449.65 998.12,699.05C964.778,821.823 899.519,933.607 809,1023L292.27,512L809,1Z"
       fill="currentColor"
     />
     <rect x="1" y="1" width="218" height="1022" fill="currentColor" />
-  </svg>,
-  'Kuadrant',
+  </SvgIcon>
 );
 
 export default KuadrantIcon;
