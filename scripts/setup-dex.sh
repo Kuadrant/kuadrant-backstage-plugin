740b8d3a6060818d3a3ccd0f74b425cbf37d9c57
emmaaroche <eroche@redhat.com>
2026-07-17 15:26:24 +0100

    fix: pin dex image to last known working version for e2e tests
    Signed-off-by: emmaaroche <eroche@redhat.com>
    Signed-off-by: R-Lawton <rlawton@redhat.com>

--- scripts/setup-dex.sh
+++ scripts/setup-dex.sh
@@ -12,7 +12,7 @@ mkdir -p kuadrant-dev-setup/dex/web/templates
 # extract dex web files from container image
 docker run --rm --user "$(id -u):$(id -g)" \
   -v "$(pwd)/kuadrant-dev-setup/dex:/tmp/out" \
-  ghcr.io/dexidp/dex:latest \
+  ghcr.io/dexidp/dex:v2.45.1 \
   sh -c '
     cp -r /srv/dex/web/static /srv/dex/web/themes /srv/dex/web/robots.txt /tmp/out/web/
     for f in /srv/dex/web/templates/*.html; do
